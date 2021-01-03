b2087676a918dd7560ba7adb880cdf26baf0f731
akshay0240 <asangani705@rku.ac.in>
2021-01-03 12:30:05 +0530

    modulation of page

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Contact extends CI_Controller {
 
 
 	public function __construct()
@@ -13,7 +13,7 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('home');
+		$this->load->view('contact');
 	}
 
 		
