66343405c1569b0d3dfc0ea59ba93504c7464ea8
akshay0240 <asangani705@rku.ac.in>
2021-01-06 16:08:35 +0530

    change 3 page

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -20,7 +20,7 @@
     <div class="container d-flex justify-content-between">
 
       <div class="logo">
-        <h1 class="text-light"><a href="index.html"><span>Advansity IT World</span></a></h1>
+        <h1 class="text-light"><a href="index.html"><span><img src="assets/images/LogoS.png" alt=""></span></a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
